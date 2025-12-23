79f09aeefcf8091570bc13d811004b8a04ae3a6a
Gammy98 <dnqorgh0410@naver.com>
2025-12-23 17:29:48 +0900

    Solve : [baekjoon] 1475,3273

--- src/baekjoon/array/p1475/Main.java
+++ src/baekjoon/array/p1475/Main.java
@@ -1,4 +1,35 @@
 package baekjoon.array.p1475;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
+    static int N;
+    static int[] num = new int[10];
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        String numString = String.valueOf(N);
+        for (int i = 0; i < numString.length(); i++) {
+            num[numString.charAt(i)-'0']++;
+        }
+
+        int add69 = num[6] + num[9];
+        if (add69 % 2 != 0) {
+            add69 = (num[6]+num[9])/2 + 1;
+        }
+        else{
+            add69 = (num[6]+num[9])/2;
+        }
+        num[6] = add69;
+        num[9] = add69;
+
+        int ans = 0;
+        for (int i : num) {
+            ans = Math.max(ans, i);
+        }
+
+        System.out.print(ans);
+    }
 }
--- src/baekjoon/array/p3273/Main.java
+++ src/baekjoon/array/p3273/Main.java
@@ -1,4 +1,32 @@
 package baekjoon.array.p3273;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+    static int n,x;
+    static int[] arr;
+    static boolean[] number = new boolean[2000001];
+    // 각 자연수의 존재 여부를 저장하는 배열, 아래에서 x-i가 1000000보다 큰 경우를 예외처리하기 싫어서 그냥 배열을 최대 200만으로 잡음
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        x = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+        for (int i : arr) {
+            if (x - i > 0 &&  number[x - i]) { //조건식은 좌측부터 확인한다 !! 이런경우 x-i가 음수가 되면 안되므로 순서중요!
+                ans++;
+            }
+            number[i] = true; //이 선언을 조건문 뒤에 둬야 i + i = x와 같은경우 예외 방지가능
+        }
+        System.out.print(ans);
+    }
 }
